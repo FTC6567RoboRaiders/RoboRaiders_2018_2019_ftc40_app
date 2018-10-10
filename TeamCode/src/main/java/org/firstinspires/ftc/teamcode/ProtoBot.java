b9baf05c9558e041fc03b6ceb4cd7f9f470545d9
FTC6567RoboRaiders <smkocik23@yahoo.com>
2018-10-10 16:23:20 -0400

    Added Protobot and ProtoDriveBot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProtoBot.java
@@ -0,0 +1,111 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+/**
+ * Created by Steve Kocik.
+ */
+
+public class ProtoBot {
+
+    /* Robot Motors, Servos, CR Servos and Sensors */
+    public DcMotor motorFrontLeft = null;
+    public DcMotor motorFrontRight = null;
+    public DcMotor motorBackLeft = null;
+    public DcMotor motorBackRight = null;
+    public BNO055IMU imu;
+
+    /* Local OpMode Members */
+    public HardwareMap hwMap =  null;
+
+    /* Public Variables */
+    public BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+    public Orientation angles;
+    public double integratedZAxis;
+    public double iza_lastHeading = 0.0;
+    public double iza_deltaHeading;
+    public float iza_newHeading;
+    public Orientation iza_angles;
+
+    /** Constructor for Robot class, current does nothing but is needed since every class needs a constructor
+     *
+     */
+    public ProtoBot(){
+
+    }
+
+    /**
+     * This method will initialize the robot
+     *
+     * @param ahwMap hardware map for the robot
+     */
+    public void initialize(HardwareMap ahwMap) {
+
+        // Save reference to hardware map
+        hwMap = ahwMap;
+
+        // Define and initialize motors
+        motorFrontLeft = hwMap.get(DcMotor.class, "leftMotorFront");
+        motorFrontRight = hwMap.get(DcMotor.class, "rightMotorFront");
+        motorBackLeft = hwMap.get(DcMotor.class, "leftMotorBack");
+        motorBackRight = hwMap.get(DcMotor.class, "rightMotorBack");
+
+        // Defines the directions the motors will spin
+        motorFrontLeft.setDirection(DcMotor.Direction.REVERSE);
+        motorFrontRight.setDirection(DcMotor.Direction.FORWARD);
+        motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
+        motorBackRight.setDirection(DcMotor.Direction.FORWARD);
+
+        // Set all motors to zero power
+        motorFrontRight.setPower(0);
+        motorFrontLeft.setPower(0);
+        motorBackRight.setPower(0);
+        motorBackLeft.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODER if encoders are installed, and we wouldn't use encoders for teleop, even if we
+        motorFrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); //will use them in teleop.
+        motorFrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorBackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorBackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Define and initialize sensors
+        imu = hwMap.get(BNO055IMU.class, "imu");
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        imu.initialize(parameters);
+    }
+
+
+    /**
+     * This method will set the power for the drive motors
+     *
+     * @param leftFront power setting for the left front motor
+     * @param rightFront power setting for the right front motor
+     * @param leftBack power setting for the left back motor
+     * @param rightBack power setting for the right back motor
+     */
+    public void setDriveMotorPower(double leftFront, double rightFront, double leftBack, double rightBack){
+
+        motorFrontLeft.setPower(leftFront);
+        motorFrontRight.setPower(rightFront);
+        motorBackLeft.setPower(leftBack);
+        motorBackRight.setPower(rightBack);
+    }
+
+
+
+}
+
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProtoDriveBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProtoDriveBot.java
@@ -7,43 +7,22 @@ import com.qualcomm.robotcore.util.Range;
 import RoboRaiders.reference.IndieRobot;
 
 /**
- *
+ *  Created by Steve Kocik
  */
 
 @TeleOp
 
 public class ProtoDriveBot extends OpMode {
 
-    public IndieRobot robot = new IndieRobot();
+    public ProtoBot robot = new ProtoBot();
 
     /* Define variables */
     float LeftBack;   // Power for left back motor
     float RightBack;  // Power for right back motor
     float LeftFront;  // Power for left front motor
     float RightFront; // Power for right front motor
-    float glyphLift;  // Power for the glyph lift motor
-    float relic;      // Power for relic motor
     float maxpwr;     // Maximum power of the four motors
     double powerFactor = 1;
-    boolean nudging = false;
-    int nudgeCount = 0;
-    int timesPivoted = 0;
-    public boolean currStateRightBumper1 = false;
-    public boolean prevStateRightBumper1 = false;
-    public boolean currStateLeftBumper1  = false;
-    public boolean prevStateLeftBumper1  = false;
-    public boolean currStateRightTrigger = false;
-    public boolean prevStateRightTrigger = false;
-    public boolean currStateLeftTrigger  = false;
-    public boolean prevStateLeftTrigger  = false;
-    public boolean currStateY = false;
-    public boolean prevStateY = false;
-    public boolean currStateX = false;
-    public boolean prevStateX = false;
-    public boolean currStateB = false;
-    public boolean prevStateB = false;
-    public boolean currStateA = false;
-    public boolean prevStateA = false;
 
     @Override
     public void init() {
@@ -57,7 +36,7 @@ public class ProtoDriveBot extends OpMode {
     @Override
     public void start() {
 
-        robot.initializeServosTeleOp();
+     //   robot.initializeServosTeleOp();
     }
 
     @Override
@@ -81,204 +60,13 @@ public class ProtoDriveBot extends OpMode {
         LeftFront = (float) scaleInput(LeftFront);
         RightFront = (float) scaleInput(RightFront);
 
-        robot.setDriveMotorPower(LeftFront * 0.95 * powerFactor, RightFront * 0.95 * powerFactor,
-                LeftBack * 0.95 * powerFactor, RightBack * 0.95 * powerFactor);
+        robot.setDriveMotorPower(LeftFront * 0.95,
+                                RightFront * 0.95,
+                                 LeftBack * 0.95,
+                                RightBack * 0.95);
 
 
-        // "Power Factor" functionality
-        currStateLeftBumper1 = gamepad1.left_bumper;
-        if (currStateLeftBumper1 && currStateLeftBumper1 != prevStateLeftBumper1) {
-
-            powerFactor = 0.5;
-            prevStateLeftBumper1 = currStateLeftBumper1;
-        }
-        else if (!currStateLeftBumper1 && currStateLeftBumper1 != prevStateLeftBumper1) {
-
-            prevStateLeftBumper1 = currStateLeftBumper1;
-        }
-        currStateRightBumper1 = gamepad1.right_bumper;
-        if (currStateRightBumper1 && currStateRightBumper1 != prevStateRightBumper1) {
-
-            powerFactor = 1;
-            prevStateRightBumper1 = currStateRightBumper1;
-        }
-        else if (!currStateRightBumper1 && currStateRightBumper1 != prevStateRightBumper1) {
-
-            prevStateRightBumper1 = currStateRightBumper1;
-        }
-
-
-        // "Nudging" functionality
-        if (gamepad1.dpad_up || gamepad1.dpad_down || gamepad1.dpad_left || gamepad1.dpad_right) { // "If any
-            // of the dpad buttons are pressed on the
-            // first controller...
-
-            if (!nudging) { // ...if nudging is false (the robot is still being nudged)...
-
-                if (gamepad1.dpad_up) robot.setDriveMotorPower(0.75, 0.75, 0.75, 0.75); // ...if 'up' was pressed, the robot moves forward...
-
-                else if (gamepad1.dpad_down) robot.setDriveMotorPower(-0.75, -0.75, -0.75, -0.75); // ...else if 'down' was pressed, the robot
-                    // moves backward...
-
-                else if (gamepad1.dpad_left) robot.setDriveMotorPower(-0.8, 0.8, 0.8, -0.8); // ...else if 'left' was pressed, the robot
-                    // strafes left...
-
-                else if (gamepad1.dpad_right) robot.setDriveMotorPower(0.8, -0.8, -0.8, 0.8); // ...else if 'right' was pressed, the robot
-                // strafes right...
-            }
-
-            nudgeCount++; // ...after this one loop cycle, the number of the nudgeCount goes up by one...
-
-            if (nudgeCount > 5) { // ...if the number of the nudgeCount goes above 5...
-
-                nudging = true; // ...nudging is true, so the robot cannot nudge anymore. This allows for 5 loop cycles
-                // of movement...
-            }
-        }
-        else { // ...else if nudging is true (the robot is no longer being nudged)...
-
-            nudging = false; // ...nudging is returned to false, which allows nudging again if any of the dpad buttons
-            // are pressed on the first controller...
-
-            nudgeCount = 0; // ...and nudgeCount is reset to 0."
-        }
-
-
-        // "Set Glyph Lift Motor Power" functionality
-        glyphLift = -gamepad2.right_stick_y;
-        glyphLift = Range.clip(glyphLift, -1, 1);
-        glyphLift = (float) scaleInput(glyphLift);
-        robot.setGlyphLiftMotorPower(glyphLift * 0.90);
-
-
-        // "Set Glyph Intake Motor Power" functionality
-        if (gamepad2.right_bumper) {
-
-            robot.setGlyphIntakeMotorPower(1.0);
-        }
-        else if (gamepad2.left_bumper){
-
-            robot.setGlyphIntakeMotorPower(-0.75);
-        }
-        else {
-
-            robot.setGlyphIntakeMotorPower(0.0);
-        }
-
-
-        // "Glyph Pivot Carry/Deposit" functionality
-        if (gamepad2.right_trigger > 0.5) {
-
-            currStateRightTrigger = true;
-        }
-        else {
-
-            currStateRightTrigger = false;
-        }
-        if (currStateRightTrigger && currStateRightTrigger != prevStateRightTrigger) {
-
-            timesPivoted++;
-            if (timesPivoted % 2 == 1) {  // If timesPivoted is an odd number (it has been pressed once, three times, five times...)
-
-                robot.glyphPivotDeposit();
-            }
-            else if (timesPivoted % 2 == 0) { // If timesPivoted is an even number (it has been pressed twice, four times, six times...)
-
-                robot.glyphPivotCarry();
-            }
-            prevStateRightTrigger = currStateRightTrigger;
-        }
-        else if (!currStateRightTrigger && currStateRightTrigger != prevStateRightTrigger) {
-
-            prevStateRightTrigger = currStateRightTrigger;
-        }
-
-
-        // "Glyph Pivot Rest" functionality
-        if (gamepad2.left_trigger > 0.5) {
-
-            currStateLeftTrigger = true;
-        }
-        else {
-
-            currStateLeftTrigger = false;
-        }
-        if (currStateLeftTrigger && currStateLeftTrigger != prevStateLeftTrigger) {
-
-            robot.glyphPivotRest();
-            prevStateLeftTrigger = currStateLeftTrigger;
-        }
-        else if (!currStateLeftTrigger && currStateLeftTrigger != prevStateLeftTrigger) {
-
-            prevStateLeftTrigger = currStateLeftTrigger;
-        }
-
-
-        // "Set Relic Motor Power" functionality
-        relic = gamepad2.left_stick_y;
-        relic = Range.clip(relic, -1, 1);
-        relic = (float) scaleInput(relic);
-
-        // If power is negative, then 1/2 the power for the arm coming back, else use 3/4 of the power.
-        if (relic > 0.0) {
-            robot.setRelicMotorPower(relic * 0.5);
-        } else {
-            robot.setRelicMotorPower(relic * 0.75);
-        }
-
-
-
-        // "Relic Wrist Up" functionality
-        currStateY = gamepad2.y;
-        if (currStateY && currStateY != prevStateY) {
-
-            robot.wristUp();
-            prevStateY = currStateY;
-        }
-        else if (!currStateY && currStateY != prevStateY) {
-
-            prevStateY = currStateY;
-        }
-
-
-        // "Relic Wrist Down" functionality
-        currStateA = gamepad2.a;
-        if (currStateA && currStateA != prevStateA) {
-
-            robot.wristDown();
-            prevStateA = currStateA;
-        }
-        else if (!currStateA && currStateA != prevStateA) {
-
-            prevStateA = currStateA;
-        }
-
-
-        // "Relic Gripper Close" functionality
-        currStateX = gamepad2.x;
-        if (currStateX && currStateX != prevStateX) {
-
-            robot.gripperClose();
-            prevStateX = currStateX;
-        }
-        else if (!currStateX && currStateX != prevStateX) {
-
-            prevStateX = currStateX;
-        }
-
-
-        // "Relic Gripper Open" functionality
-        currStateB = gamepad2.b;
-        if (currStateB && currStateB != prevStateB && !gamepad2.start) {
-
-            robot.gripperOpen();
-            prevStateB = currStateB;
-        }
-        else if (!currStateB && currStateB != prevStateB) {
-
-            prevStateB = currStateB;
-        }
-    }
+           }
 
     @Override
     public void stop() {
