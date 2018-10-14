28307ac961fa4ec926580f3e1785985c9920afbc
Jason <jason1011>
2018-10-14 11:51:49 -0700

    Added Two methodss for PID in RoboRaidersPID

--- TeamCode/src/main/java/RoboRaiders/AutoOptions/RoboRaidersPID.java
+++ TeamCode/src/main/java/RoboRaiders/AutoOptions/RoboRaidersPID.java
@@ -1,24 +1,40 @@
 package RoboRaiders.AutoOptions;
 
 public class RoboRaidersPID {
-    public float Kp = 0;
-    public float Ki = 0;
-    public float Kd = 0;
+    public double Kp = 0;
+    public double Ki = 0;
+    public double Kd = 0;
 
 
+ public void pidWithCounts (){
+     While(encoder") {
+             error = ("Target Value") - ("Sensor Value");
+     integral = integral + error;
 
-    While("Condition") {
-        error = ("Target Value") - ("Sensor Value");
-        integral = integral + error;
-
-        if (error = 0) {
-            integral = 0;
-        }
-        if (abs(error) > "adjust this") {
-            integral = 0;
-        }
-        derivative = previous_error;
-        previous_error = error;
-        power = Kp*error + Ki*integral + Kd*dirvative;
+     if (error = 0) {
+         integral = 0;
+     }
+     if (abs(error) > 100) {
+         integral = 0;
+     }
+     derivative = error - previous_error;
+     previous_error = error;
+     power = Kp*error + Ki*integral + Kd*dirvative;
     }
+ public void pidWithDistance () {
+     While(encoder) {
+             error = ("Sensor Value") - ("Target Value");
+     integral = integral + error;
+
+     if (error = 0) {
+         integral = 0;
+     }
+     if (abs(error) > 100) {
+         integral = 0;
+     }
+     derivative = error - previous_error;
+     previous_error = error;
+     power = Kp*error + Ki*integral + Kd*dirvative;
+ }
+}
 }
