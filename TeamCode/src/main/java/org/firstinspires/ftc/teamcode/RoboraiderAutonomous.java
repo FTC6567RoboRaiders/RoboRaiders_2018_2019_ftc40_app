fa6682cf2f1dd17fea65aee5d85fca478380ee6d
Jason <jason1011>
2018-10-28 12:10:16 -0700

    updated Roboraidersautonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoboraiderAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoboraiderAutonomous.java
@@ -7,23 +7,26 @@ import RoboRaiders.AutoOptions.RoboRaidersPID;
 
 @Autonomous public abstract class RoboraiderAutonomous extends LinearOpMode {
 
-   ProtoBot robot = new ProtoBot();
-   RoboRaidersPID drivePID = new RoboRaidersPID();
+    ProtoBot robot = new ProtoBot();
+    RoboRaidersPID drivePID = new RoboRaidersPID();
 
-   public double motor_power;
-   private double Target;
+    public double motor_power;
+    private double Target;
 
 
-   public void EncoderDrivePID () {
-       while (opModeIsActive() && robot.getSortedEncoderCount() > Target) {
-           motor_power = drivePID.pidWithCounts(1000,robot.getSortedEncoderCount());
-           robot.setDriveMotorPower(motor_power, motor_power, motor_power, motor_power);
-       }
+    public void EncoderDrivePID() {
+        while (opModeIsActive() && robot.getSortedEncoderCount() > Target) {
+            motor_power = drivePID.pidWithCounts(1000, robot.getSortedEncoderCount());
+            robot.setDriveMotorPower(motor_power, motor_power, motor_power, motor_power);
+        }
     }
-    public void DistanceDrivePID () {
+
+    public void DistanceDrivePID() {
         while (opModeIsActive() && sensorDistance > Target) {
-            motor_power = drivePID.pidWithDistance(0,1000);
+            motor_power = drivePID.pidWithDistance(0, 1000);
             robot.setDriveMotorPower(motor_power, motor_power, motor_power, motor_power);
 
 
+        }
+    }
 }
